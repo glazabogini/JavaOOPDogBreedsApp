ccdfcefd05c6b6861cb1d6b82fc4ed8ce785f08a
glazabogini <mikhail.nikishin.04@mail.ru>
2023-10-18 19:57:02 -0400

    Made this fetching data from config file.

--- src/main/java/com/example/dogbreeds/DatabaseConnector.java
+++ src/main/java/com/example/dogbreeds/DatabaseConnector.java
@@ -1,15 +1,33 @@
 package com.example.dogbreeds;
 
+import java.io.IOException;
+import java.util.Properties;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class DatabaseConnector {
-    private static final String URL = "jdbc:mysql://localhost:5208/dogbreedsdatabase";
-    private static final String USER = "root";
-    private static final String PASS = "1237";
+    private static Properties properties = new Properties();
+
+    static {
+        // Load configuration from properties file
+        try (InputStream input = DatabaseConnector.class.getClassLoader().getResourceAsStream("config.properties.txt")) {
+            //load a properties file from class path, inside static method
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public Connection connect() {
         try {
-            return DriverManager.getConnection(URL, USER, PASS);
+            // Get properties
+            String url = properties.getProperty("database.url");
+            String user = properties.getProperty("database.user");
+            String password = properties.getProperty("database.password");
+
+            return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             throw new RuntimeException("Error connecting to the database", e);
         }
